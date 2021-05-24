0c214f9e029d80e237901ca1bb00874142b306f1
AuRocKbra <4haurock@gamil.com>
2021-05-24 12:23:47 -0300

    Implementado insert front-end

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,25 +3,49 @@ var inicio = new Vue({
     data: {
         funcionarios: [],
         listaProdutosHeader: [
+        	{sortable: false, key: "id", label:"Id"},
 			{sortable: false, key: "nome", label:"Nome"},
 			{sortable: false, key: "setor.nome", label:"Setor"},
 			{sortable: false, key: "salario", label:"Salário"},
 			{sortable: false, key: "email", label:"Email"},
 			{sortable: false, key: "idade", label:"Idade"}
-		]
+		],
     },
     created: function(){
         let vm =  this;
-        vm.buscaProdutos();
+        vm.listaFuncionarios();
     },
     methods:{
-        buscaProdutos: function(){
+        listaFuncionarios: function(){
 			const vm = this;
 			axios.get("/funcionarios")
 			.then(response => {vm.funcionarios = response.data;
 			}).catch(function (error) {
 				vm.mostraAlertaErro("Erro interno", "Não foi possível listar funcionários");
 			}).finally(function() {
+			});
+		}
+    }
+});
+var cadastro = new Vue({
+	el:"#form-newFuncionario",
+	data:{
+		form:{
+			nome:"",
+			setor:{
+				id:"1",
+				nome:""
+			},
+			salario:"",
+			email:"",
+			idade:""
+		}
+	},
+	methods:{
+		addFuncionario(){
+			const vm = inicio;
+			axios.post("/funcionarios",this.form).then(response => {
+				vm.listaFuncionarios(); 
 			});
 		},
     }
